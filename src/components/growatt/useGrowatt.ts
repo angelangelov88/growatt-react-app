7f20e7124504342435ce0ac7b72d1a3473ce6977
Angel <angel.levski@gmail.com>
2024-10-07 08:47:19 +0100

    Update useGrowatt.ts

--- src/components/growatt/useGrowatt.ts
+++ src/components/growatt/useGrowatt.ts
@@ -53,20 +53,20 @@ function useGrowatt() {
 			// param17 = end time minute for third period
 			// param18 = 1/0 for enable/disable for third period
 
-			// const response = await fetch('http://server.growatt.com/v1/auth/login', {
-
 			// This is the POST request to set the battery to charge on cheap night rate
 			// const response = await fetch('https://server.growatt.com/tcpSet.do$action=mixSet&serialNum=KTM0CML008&type=mix_ac_charge_time_period&param1=25&param2=95&param3=1&param4=23&param5=30&param6=05&param7=30&param8=1&param9=00&param10=00&param11=00&param12=00&param13=0&param14=00&param15=00&param16=00&param17=00&param18=0', {
-
+        
 			// This is the POST request to set the battery to discharge
-			const response = await fetch('https://server.growatt.com/tcpSet.do$action=mixSet&serialNum=KTM0CML008&type=mix_ac_discharge_time_period&param1=100&param2=20&param3=22&param4=20&param5=23&param6=20&param7=1&param8=00&param9=00&param10=00&param11=00&param12=0&param13=00&param14=00&param15=00&param16=00&param17=0', {
-				method: 'POST', // Make sure the method matches what the API expects
+			// const response = await fetch('https://server.growatt.com/tcpSet.do$action=mixSet&serialNum=KTM0CML008&type=mix_ac_discharge_time_period&param1=100&param2=20&param3=22&param4=20&param5=23&param6=20&param7=1&param8=00&param9=00&param10=00&param11=00&param12=0&param13=00&param14=00&param15=00&param16=00&param17=0', {
+			const response = await fetch('http://server.growatt.com/oauth/token', {
+				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
 				},
+				// mode: 'no-cors',
 				body: JSON.stringify({
 					user_name: user,
-					password: password,
+					// password: password,
 				}),
 			});
 
@@ -80,9 +80,32 @@ function useGrowatt() {
 			console.log('error', error);
 		}};
 
+	// const fetchExample = async () => {
+	// 	try {
+	// 		const result = await fetch('http://test.growatt.com/v1/login', {
+	// 			method: 'POST',
+	// 			// headers: {
+	// 			// 	'Content-Type': 'application/json',
+	// 			// },
+	// 			// mode: 'no-cors',
+	// 			body: JSON.stringify({
+	// 				user_name: 'Angel Angelov',
+	// 				password: 'solar10',
+	// 			}),
+
+	// 		});
+	// 		console.log('result', result);
+	// 		const data = await result.json();
+	// 		console.log('data', data);
+	// 	} catch (error) {
+	// 		console.log('error', error);
+	// 	}
+	// };
+
 	useEffect(() => {
 		console.log('useEffect');
 		fetchData();
+		// fetchExample();
 	}, []);
 
 	return useMemo(
