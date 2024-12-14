c623fbdb326fed46ca736daf6794ec236b523aef
Angel <angel.levski@gmail.com>
2024-12-14 17:25:24 +0000

    Update useGrowatt.ts

--- src/components/growatt/useGrowatt.ts
+++ src/components/growatt/useGrowatt.ts
@@ -28,6 +28,7 @@ function useGrowatt() {
   const user = process.env.REACT_APP_growatt_user;
   const password = process.env.REACT_APP_growatt_password;
   console.log("user", user);
+  console.log("user1", process.env.REACT_APP_growatt_user);
   console.log("password", password);
   // const options = {};
 
