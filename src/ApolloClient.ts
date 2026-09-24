4549c713c7a94cd62374bcf57a026a47ed5a244e
Angel <82048582+angelangelov88@users.noreply.github.com>
2026-09-24 11:31:47 +0100

    Delete ApolloClient.ts

--- src/ApolloClient.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: import.meta.env.VITE_OCTOPUS_API_ENDPOINT,
-  cache: new InMemoryCache(),
-});
-
-export default client;
